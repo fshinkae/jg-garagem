c0dcdc102f3de1a8a48f62e9eeb9cffbbc1cb466
fshinkae <felipeshinkae97@gmail.com>
2023-11-27 20:45:11 -0400

    cors

--- src/main/java/com/example/garagem/util/JwtAuthenticationFilter.java
+++ src/main/java/com/example/garagem/util/JwtAuthenticationFilter.java
@@ -4,11 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
@@ -24,7 +20,6 @@ import java.util.Collections;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private JwtTokenProvider jwtTokenProvider;
-
 	private UserDetailsService userDetailsService;
 
 	public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserDetailsService userDetailsService) {
@@ -48,40 +43,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 		CorsFilter corsFilter = new CorsFilter(source);
 		corsFilter.doFilter(request, response, filterChain);
-
-		// get JWT token from http request
-		String token = getTokenFromRequest(request);
-
-		// validate token
-		if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)){
-
-			// get username from token
-			String username = jwtTokenProvider.getUsername(token);
-
-			// load the user associated with token
-			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
-			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-					userDetails,
-					null,
-					userDetails.getAuthorities()
-			);
-
-			authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-			SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-
-		}
-
-		filterChain.doFilter(request, response);
 	}
 
-	private String getTokenFromRequest(HttpServletRequest request){
-
+	private String getTokenFromRequest(HttpServletRequest request) {
 		String bearerToken = request.getHeader("Authorization");
 
-		if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")){
-			return bearerToken.substring(7, bearerToken.length());
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring(7);
 		}
 
 		return null;
