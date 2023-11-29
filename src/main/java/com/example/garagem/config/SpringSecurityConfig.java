d177d9b92d3aaf7fd98ad2b7f9af0f0272a3b39a
fshinkae <felipeshinkae97@gmail.com>
2023-11-28 21:50:02 -0400

    fix cors

--- src/main/java/com/example/garagem/config/SpringSecurityConfig.java
+++ src/main/java/com/example/garagem/config/SpringSecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -47,7 +48,7 @@ public class SpringSecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:8082"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8082", "http://localhost:8081"));
 		corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
 		corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
 		corsConfiguration.setAllowCredentials(true);
